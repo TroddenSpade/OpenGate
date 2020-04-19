f6f5949f8d16e5fe797d39d9afa11035f457cf4b
TroddenSpade <parsasam1378@gmail.com>
2020-04-19 15:10:54 +0430

    cli initial commit

---
+++ og-cli/bin/index.js
@@ -0,0 +1,137 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const axios = require("axios");
+const Table = require("cli-table");
+const ora = require("ora");
+const readline = require("readline");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const fonts = ["Slant", "Ghost", "Cricket", "standard"];
+const API = "https://opengate-20.herokuapp.com/list";
+let LIST = null;
+
+const logo = chalk.magenta(
+  figlet.textSync("Open Gate", {
+    font: fonts[Math.floor(Math.random() * 4)],
+    horizontalLayout: "default",
+    verticalLayout: "default",
+  })
+);
+
+const desc = chalk.white(
+  "In United States, Japan and most Europian countries, people do not let their governments placing such firewall, as their Constitutions state. However, it is said that some other countries have such firewall for the sake of censorship."
+);
+
+function selectVpn() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "Enter VPN No. :",
+        validate: function (value) {
+          var no = parseInt(value.match(/^[0-9]*$/));
+          if (no && no <= LIST.length && no > 0) {
+            fs.writeFile(
+              `./${LIST[no - 1]["#HostName"]}.ovpn`,
+              LIST[no - 1]["OpenVPN_ConfigData_Base64"],
+              function (err) {
+                if (err) {
+                  return console.log(err);
+                }
+
+                // run .ovpn file
+              }
+            );
+            return true;
+          }
+
+          return "Please enter a valid No.";
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(JSON.stringify(answers, null, "  "));
+    });
+}
+
+async function printPage(page) {
+  const table = new Table({
+    head: ["No.", "IP", "Score", "Ping", "Speed", "Ctry."],
+    colWidths: [5, 20, 15, 7, 15, 7],
+  });
+
+  if (page < 0) page = 0;
+  clear();
+  console.log(logo + "\n");
+  console.log(desc + "\n");
+
+  const spinner = ora({ text: "Loading Servers", discardStdin: false }).start();
+
+  setTimeout(() => {
+    spinner.color = "red";
+    spinner.text = "Loading Servers";
+  }, 1000);
+
+  if (LIST) {
+    spinner.succeed(chalk.bgRed.bold("VPN Servers :"));
+  } else {
+    LIST = await axios
+      .get(API)
+      .then((res) => {
+        spinner.succeed(chalk.bgRed.bold("VPN Servers :"));
+        return JSON.parse(res.data.list);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  for (let i = page * 10; i < page * 10 + 10; i++) {
+    if (i >= LIST.length) break;
+    table.push([
+      chalk.red(i + 1),
+      LIST[i]["IP"],
+      LIST[i]["Score"],
+      LIST[i]["Ping"],
+      LIST[i]["Speed"],
+      LIST[i]["CountryShort"],
+    ]);
+  }
+
+  console.log(table.toString());
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "theme",
+        message: "Options :",
+        choices: ["Next", "Prev", new inquirer.Separator(), "Select a VPN"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.theme) {
+        case "Next":
+          printPage(page + 1);
+          break;
+        case "Prev":
+          printPage(page - 1);
+          break;
+        default:
+          selectVpn();
+      }
+    });
+}
+
+async function main() {
+  printPage(0);
+}
+
+main();
